7b774c7592f82847ae4f003dafd90a91aad46447
Mino <aminemahi.pro@gmail.com>
2026-02-16 17:10:09 +0100

    header invert color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <header className=" absolute top-0 left-0 right-0 fixed flex justify-between items-center p-5 z-50">
+    <header className=" absolute top-0 left-0 right-0 fixed flex justify-between items-center p-5 z-50 text-white mix-blend-difference">
       <div className="">
-        <h1 className="text-2xl font-amoria text-gray-900 filter invert">Amoura</h1>
+        <h1 className="text-2xl font-amoria">Amoura</h1>
       </div>
-      <nav className="flex justify-around gap-15 filter invert">
+      <nav className="flex justify-around gap-15">
         <p>Home</p>
         <p>Menu</p>
         <p>About</p>
