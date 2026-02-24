423a71794d9d9a6b9e45d72d59b9e42f52fa8d09
Mino <aminemahi.pro@gmail.com>
2026-02-24 16:14:57 +0100

    Add content on page Menu

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import grandpa from '../assets/grandpa-historic-photo.jpg';
+
 export default function About() {
   return (
     <section>
@@ -5,9 +7,18 @@ export default function About() {
             <h1 className="text-5xl font-amoria">About</h1>
             <p className="mt-4 text-2xl font-code">Découvrez notre histoire</p>
         </div>
-        <div className="flex flex-col items-center">
-            <h2 className="text-3xl font-amoria">Notre histoire</h2>
-            <p className="mt-4 text-xl font-code max-w-2xl text-center">Amoura est née de la passion pour la cuisine et le désir de créer une expérience culinaire unique. Fondée en 2020, notre restaurant s'est rapidement imposé comme une destination incontournable pour les amateurs de gastronomie. Notre équipe de chefs talentueux s'inspire des saveurs du monde entier pour concocter des plats innovants et délicieux, tout en mettant l'accent sur la qualité des ingrédients et le respect des traditions culinaires.</p>
+        <div className="flex items-center justify-center gap-20 max-w-screen px-10 py-20">
+            <img src={grandpa} alt="Grand-père dans la cuisine" className="w-96 h-128 object-cover" />
+            <div>
+              <h2 className="text-3xl font-amoria">Notre histoire</h2>
+              <p className="mt-4 text-xl font-code max-w-2xl">
+                Amoura est née de la passion pour la cuisine et le désir de créer une expérience culinaire unique. 
+                Fondée en 2020, notre restaurant s'est rapidement imposé comme une destination incontournable pour les amateurs de gastronomie. 
+                Notre équipe de chefs talentueux s'inspire des saveurs du monde entier pour concocter des plats innovants et délicieux, 
+                tout en mettant l'accent sur la qualité des ingrédients et le respect des traditions culinaires.
+              </p>
+            </div>
+            
         </div>
     </section>
   )
