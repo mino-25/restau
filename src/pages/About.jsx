ca72841e9d42dde8a3036c38761eb03565254445
Mino <aminemahi.pro@gmail.com>
2026-02-25 17:17:25 +0100

    Text in About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
               <h2 className="text-3xl font-amoria">Notre histoire</h2>
               <p className="mt-4 text-xl font-code max-w-2xl">
                 Amoura est née de la passion pour la cuisine et le désir de créer une expérience culinaire unique. 
-                Fondée en 2020, notre restaurant s'est rapidement imposé comme une destination incontournable pour les amateurs de gastronomie. 
+                Fondée en 1920, notre restaurant s'est rapidement imposé comme une destination incontournable pour les amateurs de gastronomie. 
                 Notre équipe de chefs talentueux s'inspire des saveurs du monde entier pour concocter des plats innovants et délicieux, 
                 tout en mettant l'accent sur la qualité des ingrédients et le respect des traditions culinaires.
               </p>
